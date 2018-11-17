0981d62b7bf7fb4ccf2b3d87b16b0fb55e0ea0f4
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2018-11-18 01:06:46 +0600

    BFS Implementation done

--- src/Missionaries_Cannibals/BFS_Implementation.java
+++ src/Missionaries_Cannibals/BFS_Implementation.java
@@ -1,5 +1,81 @@
 package Missionaries_Cannibals;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BFS_Implementation {
+    private HashMap<Node,Boolean> nodeMap = new HashMap <Node,Boolean>(); //Boolean value corresponds to isExplored
+    private LinkedList<Node> nodeQueue = new LinkedList <Node>(); //BFS Queue. addLast(E e) and removeFirst() will be used as enqueue and dequeue
+
+    private long iteration;
+    private long MAX_TIME;
+    public BFS_Implementation() {
+        iteration = 0;
+
+        MAX_TIME = System.currentTimeMillis() + Main.TIMEOUT_SECONDS*1000;
+
+
+    }
+
+    public Node runBFS(){
+
+        Node initialNode = new Node(Main.m, Main.c, true, null);
+
+        nodeMap.put(initialNode, false);
+        nodeQueue.addLast(initialNode);
+
+        while (!nodeQueue.isEmpty()){
+            iteration++;
+
+            Node u = nodeQueue.removeFirst();
+            nodeMap.replace(u, true); //explored
+
+            if(u.isGoal()){
+
+                System.out.println();
+                System.out.println("***BFS RESULT***");
+                System.out.println("Number of Explored Nodes = " + iteration);
+                System.out.println("Number of Expanded Nodes = " + nodeMap.size());
+                System.out.println("Time required = " + ( ( System.currentTimeMillis() - MAX_TIME + Main.TIMEOUT_SECONDS*1000 ) ) + " ms");
+
+                System.out.println();
+                System.out.println("Printing path...");
+                u.printPath();
+                System.out.println();
+                return u;
+            }
+
+            //checking maximum iteration constraint
+            if(iteration > Main.MAX_ITERATIONS){
+                System.out.println("Maximum iterations reached in BFS. Terminating...");
+                return null;
+            }
+
+            //checking timeout constraint
+            if(System.currentTimeMillis() > MAX_TIME){
+                System.out.println("Timeout reached in BFS. Terminating...");
+                return null;
+            }
+
+
+            ArrayList <Node> childNodes = u.generateChildNodes();
+
+            for (Node v: childNodes) {
+                //System.out.println("Child" + v);
+                if (!nodeMap.containsKey(v)){
+                    nodeMap.put(v, false);
+                    nodeQueue.addLast(v);
+                }
+            }
+
+        }
+
+        //no path found from initial node to goal node
+        System.out.println("Solution not found");
+        return null;
+    }
+
 
 }
--- src/Missionaries_Cannibals/Node.java
+++ src/Missionaries_Cannibals/Node.java
@@ -1,5 +1,6 @@
 package Missionaries_Cannibals;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Node {
@@ -7,10 +8,15 @@ public class Node {
     int left_c;
     boolean boatInLeftSide;
 
-    public Node(int left_m, int left_c, boolean boatInLeftSide) {
+    Node parent;
+
+    public Node(int left_m, int left_c, boolean boatInLeftSide, Node p) {
         this.left_m = left_m;
         this.left_c = left_c;
         this.boatInLeftSide = boatInLeftSide;
+
+        parent = p;
+
     }
 
     public int getLeft_m() {
@@ -38,7 +44,7 @@ public class Node {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) {   //p not considered
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Node node = (Node) o;
@@ -49,11 +55,11 @@ public class Node {
 
     @Override
     public int hashCode() {
-        return Objects.hash(left_m, left_c, boatInLeftSide);
+        return Objects.hash(left_m, left_c, boatInLeftSide); //p not considered
     }
 
     @Override
-    public String toString() {
+    public String toString() {  //remaining
         return "Node{" +
                 "left_m=" + left_m +
                 ", left_c=" + left_c +
@@ -61,13 +67,63 @@ public class Node {
                 '}';
     }
 
-    boolean checkValidity(){
-        if(left_c>left_m || (Main.c-left_c) > (Main.m-left_m) ){
-            return false;
-        }
-        else{
+    boolean isGoal(){
+        return ( left_m == 0 && left_c ==0 ) ;
+    }
+
+    boolean isValid(){
+        if(left_m <= Main.m && left_c <= Main.c && left_m>=0 && left_c>=0 && ( left_m >= left_c  || left_m==0) && ( (Main.m-left_m) >= (Main.c-left_c) || (Main.m-left_m)==0 ) )
             return true;
+        else
+            return false;
+    }
+
+    ArrayList <Node> generateChildNodes(){  //remaining
+        //System.out.println("In generateChildNodes");
+        ArrayList <Node> children = new ArrayList <Node>();
+
+        for(int i=1; i<=Main.cap; i++){ //i denotes how many passengers
+
+            //System.out.println("In outer loop with i=" + i);
+
+            for(int j=0; j<=i; j++ ){   //j denotes missionaries carried by boat
+                //j missionaries and (i-j) cannibals carried by boat
+
+                //System.out.println("In inner loop with j=" + j);
+
+                Node newNode = null;
+
+                if(boatInLeftSide){ //carry passengers to right side
+                    newNode = new Node(left_m-j, left_c - (i-j), !boatInLeftSide, this);
+                    if(newNode.isValid()){
+                        children.add(newNode);
+                    }
+                }
+                else{ //carry passengers to left side
+                    newNode = new Node(left_m + j, left_c + (i-j), !boatInLeftSide, this);
+                    if(newNode.isValid()){
+                        children.add(newNode);
+                    }
+                }
+
+            }
+
         }
+
+//        for (Node n: children) {
+//            System.out.println("In generateChildNodes(), Child: " + n);
+//        }
+
+        return children;
+    }
+
+    void printPath(){
+        if(parent!=null){
+            parent.printPath();
+        }
+
+        System.out.println(this);
+
     }
 
 
