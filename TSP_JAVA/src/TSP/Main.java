e1291927cdb9be32a6fa03b96a05df0a844044a2
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2019-01-07 21:37:03 +0600

    abstract class named Solution written

--- TSP_JAVA/src/TSP/Main.java
+++ TSP_JAVA/src/TSP/Main.java
@@ -1,10 +1,15 @@
 package TSP;
 
+import javafx.util.Pair;
+
+import javax.sound.midi.Soundbank;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    public static int n;
 
     public static void main(String []args){
         System.out.println("Travelling Salesman Problem");
@@ -21,9 +26,40 @@ public class Main {
 
             Scanner scanner = new Scanner(new FileInputStream(input));
 
-            while(scanner.hasNextLine()){
-                System.out.println(scanner.nextLine());
+//            while(scanner.hasNextLine()){
+//                System.out.println(scanner.nextLine());
+//            }
+
+            System.out.println("Taking Inputs...");
+
+            n = scanner.nextInt();
+            System.out.println(n + " cities in the problem.\nLocations are:");
+
+            ArrayList<Pair<Integer, Integer>> locations = new ArrayList<Pair<Integer, Integer>>();
+
+            int x, y;
+            for (int i = 0; i < n; i++) {
+                x = scanner.nextInt();
+                y = scanner.nextInt();
+
+                locations.add(new Pair<>(x, y));
+                System.out.println(locations.get(i).getKey() + " " + locations.get(i).getValue());
+
             }
+
+            System.out.println("Calculating Distances...");
+
+            double [][] distanceArray = new double[n][n];
+            for (int i = 0; i < n; i++) {
+                int x1 = locations.get(i).getKey();
+                int y1 = locations.get(i).getValue();
+                for (int j = 0; j < n; j++) {
+                    int x2 = locations.get(j).getKey();
+                    int y2 = locations.get(j).getValue();
+                    distanceArray[i][j] = Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );
+                }
+            }
+
 
 
 
---
+++ TSP_JAVA/src/TSP/Solution.java
@@ -0,0 +1,54 @@
+package TSP;
+
+import java.util.ArrayList;
+
+public abstract class Solution {
+    ArrayList<Integer> tourCityIdx;
+    double duration;
+    double totalDistanceTravelled;
+    int numberOfRoadsTravelled;
+
+    Solution(){
+        tourCityIdx = new ArrayList<Integer>();
+        duration = 0;
+        totalDistanceTravelled = 0;
+        numberOfRoadsTravelled = 0;
+    }
+
+    public ArrayList<Integer> getTourCityIdx() {
+        return tourCityIdx;
+    }
+
+    public void setTourCityIdx(ArrayList<Integer> tourCityIdx) {
+        this.tourCityIdx = tourCityIdx;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public double getTotalDistanceTravelled() {
+        return totalDistanceTravelled;
+    }
+
+    public void setTotalDistanceTravelled(double totalDistanceTravelled) {
+        this.totalDistanceTravelled = totalDistanceTravelled;
+    }
+
+    public int getNumberOfRoadsTravelled() {
+        return numberOfRoadsTravelled;
+    }
+
+    public void setNumberOfRoadsTravelled(int numberOfRoadsTravelled) {
+        this.numberOfRoadsTravelled = numberOfRoadsTravelled;
+    }
+
+    abstract void solve();
+
+    abstract void improve();
+
+}
