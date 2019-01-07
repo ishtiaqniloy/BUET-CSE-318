6216e397615486b8909e16fcfaa6cf25e1da1742
ishtiaqniloy <1505080.aai@ugrad.cse.buet.ac.bd>
2019-01-07 21:06:06 +0600

    a

--- TSP_JAVA/src/TSP/Main.java
+++ TSP_JAVA/src/TSP/Main.java
@@ -1,11 +1,36 @@
 package TSP;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String []args){
-        System.out.println("Hello World");
+        System.out.println("Travelling Salesman Problem");
+
+        try{
+            File input = new File("Input.txt");
+            if(input.exists()){
+                System.out.println("Input file found!");
+            }
+            else{
+                System.out.println("Input file not found. Exiting...");
+                return;
+            }
+
+            Scanner scanner = new Scanner(new FileInputStream(input));
+
+            while(scanner.hasNextLine()){
+                System.out.println(scanner.nextLine());
+            }
 
 
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
 
 
